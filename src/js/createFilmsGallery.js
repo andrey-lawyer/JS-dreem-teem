9e63fa0e629590f595d12fbacaf4c7cfaf9228d5
Yurii Oborskiy <yuriioborskiy@gmail.com>
2022-11-29 20:33:32 +0200

    installed gallery render and search scripts

--- src/js/createFilmsGallery.js
+++ src/js/createFilmsGallery.js
@@ -4,9 +4,9 @@ import createCards from '../templates/filmcard.hbs';
 import { Trending } from './trending';
 import { genres } from './genres';
 
-const searchFormEl = document.querySelector('.search-form');
+const searchFormEl = document.querySelector('.js-search-form');
 const galleryEl = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
+// const loadMoreBtn = document.querySelector('.load-more');
 const filmSearch = new FilmSearch();
 const trending = new Trending();
 
@@ -22,24 +22,41 @@ const onSearchFormSubmit = async event => {
 
     if (response.data.total_results === 0) {
       event.target.elements.searchQuery.value = ' ';
-      loadMoreBtn.classList.add('is-hidden');
+      // loadMoreBtn.classList.add('is-hidden');
       galleryEl.innerHTML = '';
       Notiflix.Notify.failure(
-        'Sorry, there are no films matching your search query. Please try again.'
+        'Sorry, there are no films matching your search query'
       );
 
       return;
     }
     filmSearch.total_results = response.data.total_results;
 
-    if (response.data.total_results <= filmSearch.per_page) {
-      loadMoreBtn.classList.add('is-hidden');
-    } else {
-      loadMoreBtn.classList.remove('is-hidden');
-    }
-    Notiflix.Notify.success(
-      `Hooray! We found ${response.data.total_results} films.`
-    );
+    // if (response.data.total_results <= filmSearch.per_page) {
+    //   loadMoreBtn.classList.add('is-hidden');
+    // } else {
+    //   loadMoreBtn.classList.remove('is-hidden');
+    // }
+    Notiflix.Notify.success(`Found ${response.data.total_results} films.`);
+
+    response.data.results.forEach(movie => {
+      movie.release_date = '|  ' + movie.release_date.slice(0, 4);
+      movie.original_title = movie.original_title.toUpperCase();
+
+      const newArr = [];
+      movie.genre_ids.map((element, index, array) => {
+        genres.forEach(genre => {
+          if (genre.id == element) {
+            newArr.push(genre.name);
+          }
+        });
+      });
+      if (newArr.length > 2) {
+        newArr.splice(2, newArr.length - 2, 'Other');
+      }
+      movie.genre_ids = [...newArr];
+    });
+
     galleryEl.innerHTML = createCards(response.data.results);
     //   !!!!!!
   } catch (error) {
@@ -48,10 +65,8 @@ const onSearchFormSubmit = async event => {
 };
 const onLoadMoreBtnClick = async event => {
   if (!filmSearch.isNextDataExist()) {
-    loadMoreBtn.classList.add('is-hidden');
-    Notiflix.Notify.failure(
-      "Were sorry, but you've reached the end of search results."
-    );
+    // loadMoreBtn.classList.add('is-hidden');
+    Notiflix.Notify.failure('sorry, the end of search results reached.');
     return;
   }
   filmSearch.page += 1;
@@ -80,7 +95,7 @@ const onLoadMoreBtnClick = async event => {
   });
 };
 
-// searchFormEl.addEventListener('submit', onSearchFormSubmit);
+searchFormEl.addEventListener('submit', onSearchFormSubmit);
 // loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 
 // ===========================trending====================
