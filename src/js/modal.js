e7fcd5a7325554b145c04eb539ce45bb046058c4
Andrey Uvarov <andrey-1@ukr.net>
2022-12-01 23:22:06 +0200

    local storage

--- src/js/modal.js
+++ src/js/modal.js
@@ -7,7 +7,6 @@ const refs = {
 };
 
 refs.galleryEl.addEventListener('click', toggleModal);
-
 refs.closeModalBtn.addEventListener('click', modalClose);
 
 function toggleModal(event) {
@@ -20,12 +19,20 @@ function toggleModal(event) {
   document.addEventListener('keydown', onEscDown);
   document.addEventListener('click', closeOnBackdropClick);
 }
+// правки Андрей
+const buttonWathedModal = document.querySelector('.js-Wathed');
+const buttonQueueModal = document.querySelector('.js-Queuee');
+// правки Андрей
 
 function modalClose(event) {
   refs.backdropEl.classList.add('is-hidden');
   document.removeEventListener('keydown', onEscDown);
   refs.body.classList.remove('overflow-hidden');
   removeEventListener('click', closeOnBackdropClick);
+  // правки Андрей
+  buttonWathedModal.disabled = false;
+  buttonQueueModal.disabled = false;
+  // правки Андрей
 }
 
 document.addEventListener('keydown', onEscDown);
---
+++ src/library.js
@@ -0,0 +1,3 @@
+// import './js/adFilms';
+import './js/modal';
+import './js/showFilmsLibrary';
