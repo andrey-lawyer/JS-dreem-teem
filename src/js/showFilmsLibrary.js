766e12428fdab0f12138d55a2e35fc7ad68d69ac
Andrey Uvarov <andrey-1@ukr.net>
2022-12-03 12:55:23 +0200

    comit14

--- src/js/showFilmsLibrary.js
+++ src/js/showFilmsLibrary.js
@@ -1,4 +1,5 @@
 import createCards from '../templates/filmcardLibrary.hbs';
+import Notiflix from 'notiflix';
 
 const STORAGE_KEY_WATCHED = 'wathedlist-films';
 const STORAGE_KEY_QUEUEE = 'queuee-films';
@@ -14,12 +15,45 @@ const refs = {
   buttonLibraryQueue: document.querySelector('.js-library-queue'),
 
   containerLibrary: document.querySelector('.js-gallery-library'),
-};
-refs.buttonLibraryWatched.style.backgroundColor = '#ff6b01';
-console.log(refs.containerLibrary);
 
-refs.containerLibrary.innerHTML = createCards(parsedateLocalWathed);
-// refs.containerLibrary.insertAdjacentHTML('beforeend',  createCards(parsedateLocalWathed));
+  buttonLoadMore: document.querySelector('.load-more'),
+};
+
+refs.buttonLibraryWatched.style.backgroundColor = '#ff6b01';
+// console.log(refs.containerLibrary);
+// LOAD-MORE!!!!
+refs.buttonLoadMore.style.display = 'none';
+
+if (parsedateLocalWathed.length <= 6) {
+  refs.containerLibrary.innerHTML = createCards(parsedateLocalWathed);
+} else {
+  refs.containerLibrary.innerHTML = createCards(
+    parsedateLocalWathed.slice(0, 6)
+  );
+  refs.buttonLoadMore.style.display = 'block';
+}
+
+let counterBegin = 0;
+let counterEnd = 0;
+function onClickLoadMore() {
+  counterBegin += 6;
+  counterEnd += 12;
+  if (parsedateLocalWathed.length <= counterBegin) {
+    refs.buttonLoadMore.style.display = 'none';
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query'
+    );
+  } else {
+    refs.containerLibrary.insertAdjacentHTML(
+      'beforeend',
+      createCards(parsedateLocalWathed.slice(counterBegin, counterEnd))
+    );
+  }
+}
+
+refs.buttonLoadMore.addEventListener('click', onClickLoadMore);
+
+// LOAD-MORE!!!!
 
 const onShowQueue = () => {
   refs.containerLibrary.innerHTML = createCards(parsedateLocalQueuee);
